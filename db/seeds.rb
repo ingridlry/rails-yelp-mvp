ce5fc216ac6de8b9d9cc3b00299d55bbabc4486a
ingrid leroy <ingrid.leroy1@gmail.com>
2019-02-20 14:51:10 +0100

    add file rails yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '+33 56789067',
+    category: 'chinese'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '+33 56789067',
+    category: 'italian'
+  },
+  {
+    name:         'Gladine',
+    address:      'Paris',
+    phone_number: '+33 125748950',
+    category: 'french'
+  },
+  {
+    name:         'Vert Mont',
+    address:      '13/18 Boeschepe Flandres',
+    phone_number: '+33 134994040404',
+    category: 'french'
+  },
+  {
+    name:         'Lannexe',
+    address:      'avenue de la république La madeleine',
+    phone_number: '+33 320555090',
+    category: 'french'
+  },
+  {
+    name:         'Madame Yum Yum',
+    address:      'rue des arts Lille',
+    phone_number: '+33 3444949494',
+    category: 'japanese'
+  }
+]
+Restaurant.create!(restaurants_attributes)
